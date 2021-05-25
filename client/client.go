cde34233254b490818a342c224d3ed574d0862d2
李运堂 <liyuntang@liyuntang-2.local>
2021-05-25 17:12:16 +0800

    尝试开发client端,暂时放弃

---
+++ client/client.go
@@ -0,0 +1,11 @@
+package main
+
+//func main()  {
+//	server := flag.String("h", "localhost", "cache server address")
+//	op := flag.String("c", "get", "command, could be get/set/del")
+//	key := flag.String("k", "", "key")
+//	value := flag.String("v", "", "value")
+//	flag.Parse()
+//
+//	client :=
+//}
